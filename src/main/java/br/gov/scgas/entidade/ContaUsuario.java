a6db1ba671d5b7eefeefd7f420fe5fd8da92e1b3
robertobsjunior <roberto2011.junior@gmail.com>
2016-11-09 09:41:48 -0200

    Entidades

---
+++ src/main/java/br/gov/scgas/entidade/ContaUsuario.java
@@ -0,0 +1,71 @@
+package br.gov.scgas.entidade;
+
+import java.io.Serializable;
+
+public class ContaUsuario implements Serializable{
+
+	private Long id;
+	private String nome;
+	private String email;
+	private String senha;
+	private String pinSenha;
+	private String tokenFacebook;
+	private String tokenGmail;
+	private String tokenTwiter;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getPinSenha() {
+		return pinSenha;
+	}
+	public void setPinSenha(String pinSenha) {
+		this.pinSenha = pinSenha;
+	}
+	public String getTokenFacebook() {
+		return tokenFacebook;
+	}
+	public void setTokenFacebook(String tokenFacebook) {
+		this.tokenFacebook = tokenFacebook;
+	}
+	public String getTokenGmail() {
+		return tokenGmail;
+	}
+	public void setTokenGmail(String tokenGmail) {
+		this.tokenGmail = tokenGmail;
+	}
+	public String getTokenTwiter() {
+		return tokenTwiter;
+	}
+	public void setTokenTwiter(String tokenTwiter) {
+		this.tokenTwiter = tokenTwiter;
+	}
+
+
+
+}
